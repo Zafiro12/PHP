42e560d91223bcbf251508deffd51cca5d41ce90
Zafiro12 <alexsanta1205@gmail.com>
2022-10-03 10:21:44 +0200

    snapshot 3o22

--- tema_1/ejercicios_arrays/ejercicio_10/index.php
+++ tema_1/ejercicios_arrays/ejercicio_10/index.php
@@ -6,10 +6,18 @@
 </head>
 <body>
 <?php
-    $array;
-    for ($i = 1; $i <= 10; $i++) {
-        array_push($array, $i);
+    $numeros = array(1,2,3,4,5,6,7,8,9,10);
+    $suma = 0;
+    $contador = 0;
+    foreach ($numeros as $numero) {
+        if ($numero % 2 == 0) {
+            $suma += $numero;
+            $contador++;
+        } else {
+            echo $numero . "<br>";
+        }
     }
+    echo "La media de los pares es: " . $suma / $contador;
 ?>
 </body>
 </html>
