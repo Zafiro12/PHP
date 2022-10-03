0effb2b5fa93e82d02de33a3c8762ea14147e04b
Zafiro12 <alexsanta1205@gmail.com>
2022-10-03 10:42:32 +0200

    snapshot 3o22b

--- tema_1/ejercicios_arrays/ejercicio_9/index.php
+++ tema_1/ejercicios_arrays/ejercicio_9/index.php
@@ -9,7 +9,7 @@
 $lenguajes_cliente = array('HTML' => 'Hypertext Markup Language', 'CSS' => 'Cascading Style Sheets', 'JS' => 'JavaScript', 'PHP' => 'PHP: Hypertext Preprocessor');
 $lenguajes_servidor = array('PHP' => 'PHP: Hypertext Preprocessor', 'ASP' => 'Active Server Pages', 'JSP' => 'Java Server Pages', 'CF' => 'ColdFusion');
 $lenguajes = array_merge($lenguajes_cliente, $lenguajes_servidor);
-//mostrarlo en una tabla
+
 echo "<table border='1'>";
 echo "<tr><th>Lenguaje</th><th>Descripción</th></tr>";
 foreach ($lenguajes as $key => $value) {
--- tema_1/ejercicios_string/ej2/index.php
+++ tema_1/ejercicios_string/ej2/index.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <title>Ejercicio</title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
+
 <body>
-    
+    <form action="index.php" method="post">
+        <label for="palabra">Introduce una palabra:</label>
+        <input type="text" name="palabra" id="palabra" required>
+        <input type="submit" value="Comprobar" name="submit">
+    </form>
+    <?php
+    if (isset($_POST['submit'])) {
+        $palabra = $_POST['palabra'];
+        if (strrev($palabra) == $palabra) {
+            echo "<p>La palabra $palabra es palindromo</p>";
+        } else {
+            echo "<p>La palabra $palabra no es palindromo</p>";
+        }
+    }
+    ?>
 </body>
+
 </html>
