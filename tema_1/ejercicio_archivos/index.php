e8eec78b35137dfe5d885139a07e41d7e885618e
Zafiro12 <alexsanta1205@gmail.com>
2022-10-10 10:30:54 +0200

    10o22a

--- tema_1/ejercicio_archivos/index.php
+++ tema_1/ejercicio_archivos/index.php
@@ -9,37 +9,27 @@
 <form action="recepcion.php" method="post" enctype="multipart/form-data">
     <label for="nombre">Nombre:<br></label>
     <input type="text" name="nombre" id="nombre">
-    <br>
+    <br><br>
 
-    <label for="apellidos">Apellidos:<br></label>
-    <input type="text" name="apellidos" id="apellidos">
-    <br>
+    <label for="usuario">Usuario:<br></label>
+    <input type="text" name="usuario" id="usuario">
+    <br><br>
 
     <label for="pwd">Contraseña:<br></label>
     <input type="password" name="pwd" id="pwd">
-    <br>
+    <br><br>
 
     <label for="nif">DNI<br></label>
     <input type="text" name="nif" id="nif" maxlength="9" minlength="9">
-    <br>
+    <br><br>
 
     <label for="sexo">Sexo<br></label>
     <input type="radio" name="sexo" value="Hombre" id="sexo">Hombre<br>
     <input type="radio" name="sexo" value="Mujer" id="sexo">Mujer<br>
-    <br>
+    <br><br>
 
-    <label for="foto">Incluir mi foto:</label>
+    <label for="foto">Incluir mi foto (Archivo tipo imagen máx. 500kb):</label>
     <input type="file" name="foto" id="foto">
-    <br><br>
-
-    <label for="nacido">Nacido en:</label>
-    <select name="nacido" id="nacido">
-        <option value="Málaga">Málaga</option>
-    </select>
-    <br><br>
-
-    <label for="comentarios">Comentarios:</label>
-    <textarea name="comentarios" id="comentarios" cols="30" rows="4"></textarea>
     <br><br>
 
     <input type="checkbox" name="sub" id="sub" checked="checked">
