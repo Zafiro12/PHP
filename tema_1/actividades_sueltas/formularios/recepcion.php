7b92f7e3c0c633cc42ef849aa695d221fe46fc2f
Zafiro12 <alexsanta1205@gmail.com>
2022-10-19 11:10:36 +0200

    19o22a

--- tema_1/actividades_sueltas/formularios/recepcion.php
+++ tema_1/actividades_sueltas/formularios/recepcion.php
@@ -30,6 +30,6 @@ if (isset($_POST['Guardar'])) {
 <?php
 }
 else {
-    header("Location: index.php");
+    header("Location: index.html");
 }
 ?>
---
+++ tema_1/ejercicio_archivos/post.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Ejemplo de Formulario</title>
+</head>
+
+<body>
+
+<?php
+function letraNIF($dni): string
+{
+    return substr("TRWAGMYFPDXBNJZSQVHLCKEO", $dni % 23, 1);
+}
+
+$error_nombre = empty($_POST['nombre']) || strlen($_POST['nombre']) > 20;
+$error_usuario = empty($_POST['usuario']);
+$error_pwd = empty($_POST['pwd']) || strlen($_POST['pwd']) > 8;
+$error_nif = empty($_POST['nif']) || strlen($_POST['nif']) != 9 || letraNIF($_POST['nif']) != substr($_POST['nif'], -1);
+$error_sexo = !isset($_POST['sexo']);
+
+if (isset($_POST['enviar']) && !$error_nombre && !$error_usuario && !$error_pwd && !$error_nif && !$error_sexo) {
+    echo "Nombre: " . $_POST['nombre'] . "<br>";
+    echo "Usuario: " . $_POST['usuario'] . "<br>";
+    echo "Contraseña: " . $_POST['pwd'] . "<br>";
+    echo "NIF: " . $_POST['nif'] . "<br>";
+    echo "Sexo: " . $_POST['sexo'] . "<br>";
+    echo "Foto: " . $_FILES['foto']['name'] . "<br>";
+    echo "Suscripción: " . (isset($_POST['sub']) ? "Sí" : "No") . "<br>";
+} else {
+    ?>
+
+    <header>
+        <h1>Rellena tu CV</h1>
+    </header>
+    <main>
+        <form action="post.php" method="post" enctype="multipart/form-data">
+            <label for="nombre">Nombre:<br></label>
+            <input type="text" name="nombre" id="nombre" value="<?php
+            if (isset($_POST['enviar'])) {
+                echo $_POST['nombre'];
+            }
+            ?>"> <?php
+            if ($error_nombre) {
+                echo "<span style='color: red'>Nombre incorrecto</span>";
+            }
+            ?>
+            <br><br>
+
+            <label for="usuario">Usuario:<br></label>
+            <input type="text" name="usuario" id="usuario" value="<?php
+            if (isset($_POST['enviar'])) {
+                echo $_POST['usuario'];
+            }
+            ?>"> <?php
+            if ($error_usuario) {
+                echo "<span style='color: red'>Usuario incorrecto</span>";
+            }
+            ?>
+            <br><br>
+
+            <label for="pwd">Contraseña:<br></label>
+            <input type="password" name="pwd" id="pwd"> <?php
+            if ($error_pwd) {
+                echo "<span style='color: red'>Contraseña incorrecta</span>";
+            }
+            ?>
+            <br><br>
+
+            <label for="nif">DNI<br></label>
+            <input type="text" name="nif" id="nif" value="<?php
+            if (isset($_POST['enviar'])) {
+                echo $_POST['nif'];
+            }
+            ?>"> <?php
+            if ($error_nif) {
+                echo "<span style='color: red'>NIF incorrecto</span>";
+            }
+            ?>
+            <br><br>
+
+            <fieldset>
+                <legend>Sexo</legend>
+                <?php
+                if ($error_sexo) {
+                    echo "<span style='color: red'>Obligatorio</span><br>";
+                }
+                ?>
+                <input type="radio" name="sexo" id="hombre" value="hombre">
+                <label for="hombre">Hombre</label>
+                <br>
+                <input type="radio" name="sexo" id="mujer" value="mujer">
+                <label for="mujer">Mujer</label>
+            </fieldset>
+            <br><br>
+
+            <label for="foto">Incluir mi foto (Archivo tipo imagen máx. 500kb):</label>
+            <input type="file" name="foto" id="foto">
+            <br><br>
+
+            <input type="checkbox" name="sub" id="sub" checked>
+            <label for="sub">Subscribirse al boletín de Novedades</label>
+            <br><br>
+
+            <input type="submit" name="enviar" value="Guardar cambios">
+            <input type="reset" value="Borrar los datos introducidos">
+        </form>
+    </main>
+    <?php
+}
+?>
+</body>
+</html>
