198e45c90351b4338ce720339e0b155c8b775520
Zafiro12 <alexsanta1205@gmail.com>
2022-11-01 12:54:00 +0100

    finalizado estudios

--- src/tema_1/estudios/index.php
+++ src/tema_1/estudios/index.php
@@ -10,47 +10,86 @@
         * {
             box-sizing: border-box;
         }
+
         body {
             margin: 0;
             display: flex;
-            flex-flow: column wrap;
+            flex-flow: row wrap;
             justify-content: center;
             align-items: center;
             height: 100vh;
             width: auto;
 
             background-color: lightgray;
+
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: bold;
         }
-        div {
-            border: 1px solid black;
+
+        #resultado {
+            display: flex;
+            flex-flow: column wrap;
+            align-items: flex-start;
+            padding: 10px;
+            margin: 3%;
+
+            border: 2px solid black;
             border-radius: 3%;
+            background-color: white;
         }
+
         form {
             display: flex;
             flex-flow: column wrap;
             justify-content: center;
             align-items: center;
             padding: 25px;
-            
-            border: 1px solid black;
+
+            border: 2px solid black;
             border-radius: 3%;
             background-color: white;
-
-            font-family: Arial, Helvetica, sans-serif;
-            font-weight: bold;
         }
+
         form>div {
             margin: 10px;
-            border: none;
         }
     </style>
 </head>
 
 <body>
-    <form action="index.php" method="post">
+    <?php
+    function mantenerValor($nombre)
+    {
+        if (isset($_POST[$nombre])) {
+            echo "value='" . $_POST[$nombre] . "'";
+        }
+    }
+
+    function errorCampo($booleano)
+    {
+        if ($booleano) {
+            echo "<span style='color:red;'>*Campo Obligatorio</span>";
+        }
+    }
+
+    // Se crean las variables de los errores
+    // Es mejor inicializarlas con false
+    $error_text = false;
+    $error_radio = false;
+    $error_file = false;
+
+    if (isset($_POST['submit'])) {
+        $error_text = empty($_POST['text']);
+        $error_radio = !isset($_POST['radio']);
+        $error_file = $_FILES['file']['name'] == "" || $_FILES['file']['size'] > 500 * 1000;
+    }
+    ?>
+
+    <form action="index.php" method="post" enctype="multipart/form-data">
         <div>
             <label for="nombre">Nombre:</label>
-            <input type="text" name="texto" id="nombre">
+            <input type="text" name="text" id="nombre" <?php mantenerValor("text"); ?>>
+            <?php errorCampo($error_text) ?>
         </div>
 
         <div>
@@ -59,6 +98,8 @@
 
             <label for="mujer">Mujer</label>
             <input type="radio" name="radio" id="mujer" value="mujer">
+
+            <?php errorCampo($error_radio) ?>
         </div>
 
         <div>
@@ -73,12 +114,12 @@
 
         <div>
             <label for="descripcion">Descripción:</label>
-            <textarea name="textarea" id="descripcion" cols="30" rows="5" style="resize: none;"></textarea>
+            <textarea name="textarea" id="descripcion" cols="30" rows="5" style="resize: none;" <?php mantenerValor("textarea"); ?>></textarea>
         </div>
 
         <div>
             <label for="sub">Suscribirse</label>
-            <input type="checkbox" name="checkbox" id="sub">
+            <input type="checkbox" name="checkbox" id="sub" checked>
         </div>
 
         <div>
@@ -88,6 +129,38 @@
 
         <input type="submit" name="submit" value="Enviar">
     </form>
+
+    <?php
+    if (isset($_POST['submit']) && !$error_text && !$error_radio) {
+        echo "<div id='resultado'>";
+        echo "<p>Nombre: " . $_POST['text'] . "</p>";
+        echo "<p>Sexo: " . $_POST['radio'] . "</p>";
+        echo "<p>Provincia: " . $_POST['select'] . "</p>";
+        if (!empty($_POST['textarea'])) echo "<p>Descripción: " . $_POST['textarea'] . "</p>";
+        echo "<p>Suscripción: " . $_POST['checkbox'] . "</p>";
+
+        if (!$error_file) {
+            echo "<hr>";
+            echo "<h2>Archivo</h2>";
+            echo "<p>Nombre: " . $_FILES['file']['name'] . "</p>";
+
+            if ($_FILES['file']['type'] == "text/plain") {
+                echo "<h3>Contenido</h3>";
+                $file = fopen($_FILES['file']['tmp_name'], "r");
+
+                while (!feof($file)) {
+                    $line = fgets($file);
+                    echo "<p>$line</p>";
+                }
+
+                fclose($file);
+            }
+        }
+
+
+        echo "</div>";
+    }
+    ?>
 </body>
 
 </html>
