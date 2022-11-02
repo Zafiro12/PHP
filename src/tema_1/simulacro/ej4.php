88a776b06e560c09824f4b98656c1cdc520c3201
Zafiro12 <alexsanta1205@gmail.com>
2022-11-02 12:45:35 +0100

    hacer el metodo verGrupo

--- src/tema_1/simulacro/ej4.php
+++ src/tema_1/simulacro/ej4.php
@@ -21,6 +21,36 @@
         }
         return $n;
     }
+    function verGrupo($grupo)
+    {
+        $file = fopen("Horario/horarios2.txt", "r");
+
+        while (!feof($file)) {
+            $line = fgets($file);
+            $arr = explode("\t",$line);
+            $horario = [["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""]];
+
+            if (empty($line)) continue;
+
+            if ($arr[0] == $grupo) {
+                
+                echo "<table>";
+                echo "<tr>";
+                echo "<th></th><th>Lunes</th><th>Martes</th><th>Miércoles</th><th>Jueves</th><th>Viernes</th>";
+                echo "</tr>";
+                for ($i = 1; $i < longitud($line); $i++) {
+                    if (is_int($arr[$i])) echo "numero";
+                }
+                echo "</table>";
+            }
+        }
+        fclose($file);
+    }
 
     $subirArchivo = isset($_POST["subirArchivo"]);
     $error_archivo = false;
@@ -46,23 +76,34 @@
     <?php
     } else {
         fclose($comprobar);
+
+        $ver = isset($_POST["ver"]);
     ?>
         <h1>Ejercicio 4</h1>
         <form action="ej4.php" method="post">
-            <label for="profesor">Horario del Profesor</label>
-            <select name="profesor" id="profesor">
+            <label for="grupo">Horario del Profesor</label>
+            <select name="grupo" id="grupo">
                 <?php
-                    $file = fopen("Horario/horarios.txt", "r");
+                $file = fopen("Horario/horarios2.txt", "r");
 
-                    while(!feof($file)) {
-                        $line = fgets($file);
-                        
-                    }
-                    fclose($file);
+                while (!feof($file)) {
+                    $line = fgets($file);
+                    $arr = explode("\t",$line);
+
+                    if (empty($line)) continue;
+
+                    echo "<option value='" . $arr[0] . "'>" . $arr[0] . "</option>";
+                }
+                fclose($file);
                 ?>
             </select>
+            <input type="submit" name="ver" value="ver">
         </form>
     <?php
+        if ($ver) {
+            $grupo = $_POST["grupo"];
+            @verGrupo($grupo);
+        }
     }
     ?>
 </body>
