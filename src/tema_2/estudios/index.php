7aa400e9e950761f080239abd8e814270435bef6
Zafiro12 <alexsanta1205@gmail.com>
2022-11-14 10:25:04 +0100

    añade accion

--- src/tema_2/estudios/index.php
+++ src/tema_2/estudios/index.php
@@ -1,11 +1,12 @@
 <?php
-$enlaces = array(
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Red_X.svg/1024px-Red_X.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Edit_icon_%28the_Noun_Project_30184%29.svg/1024px-Edit_icon_%28the_Noun_Project_30184%29.svg.png",
-    "https://www.freeiconspng.com/thumbs/eye-icon/eyeball-icon-png-eye-icon-1.png"
-);
 function visualizarDatos($link, $tabla)
 {
+    $enlaces = array(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Red_X.svg/1024px-Red_X.svg.png",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Edit_icon_%28the_Noun_Project_30184%29.svg/1024px-Edit_icon_%28the_Noun_Project_30184%29.svg.png",
+        "https://www.freeiconspng.com/thumbs/eye-icon/eyeball-icon-png-eye-icon-1.png"
+    );
+
     $sql = "SELECT * FROM $tabla";
     if ($result = mysqli_query($link, $sql)) {
         if (mysqli_num_rows($result) > 0) {
@@ -16,13 +17,17 @@ function visualizarDatos($link, $tabla)
                 echo "<th>" . $fieldinfo->name . "</th>";
                 array_push($nameArray, $fieldinfo->name);
             }
+            echo "<th>Acciones</th>";
             echo "</tr>";
             while ($row = mysqli_fetch_array($result)) {
                 echo "<tr>";
                 foreach ($nameArray as $name) {
                     echo "<td>" . $row[$name] . "</td>";
                 }
-                
+                echo "<td>";
+                echo "<a href='delete.php?id=" . $row['id'] . "'><img src='" . $enlaces[0] . "' width='20px' height='20px'></a>";
+                echo "<a href='update.php?id=" . $row['id'] . "'><img src='" . $enlaces[1] . "' width='20px' height='20px'></a>";
+                echo "<a href='view.php?id=" . $row['id'] . "'><img src='" . $enlaces[2] . "' width='20px' height='20px'></a>";
                 echo "</tr>";
             }
             echo "</table>";
