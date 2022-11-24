cc1b635662527599b1d3d414330881ae9f93dcc1
Zafiro12 <alexsanta1205@gmail.com>
2022-11-24 09:05:48 +0100

    mejor visualización

--- src/tema_2/practica_9/index.php
+++ src/tema_2/practica_9/index.php
@@ -1,42 +1,46 @@
 <?php
-function visualizarDatos($link, $tabla)
-{
-    $enlaces = array(
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Red_X.svg/1024px-Red_X.svg.png",
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Edit_icon_%28the_Noun_Project_30184%29.svg/1024px-Edit_icon_%28the_Noun_Project_30184%29.svg.png",
-        "https://www.freeiconspng.com/thumbs/eye-icon/eyeball-icon-png-eye-icon-1.png"
-    );
+// Incluir archivo de configuracion
+require_once "config.php";
 
+function visualizarDatos($link, $tabla, ...$columnas)
+{
     $sql = "SELECT * FROM $tabla";
     if ($result = mysqli_query($link, $sql)) {
         if (mysqli_num_rows($result) > 0) {
             echo "<table>";
             echo "<tr>";
-            $nameArray = array();
-            while ($fieldinfo = mysqli_fetch_field($result)) {
-                if ($fieldinfo->name == "id_pelicula" || $fieldinfo->name == "titulo" || $fieldinfo->name == "caratula") {
-                    echo "<th>" . $fieldinfo->name . "</th>";
-                    array_push($nameArray, $fieldinfo->name);
+
+            //coger el nombre de las columnas
+            $sql = "SHOW COLUMNS FROM $tabla";
+            $cols = mysqli_query($link, $sql);
+
+            // Mostrar los nombres de las columnas
+            while ($col = mysqli_fetch_array($cols)) {
+                if (in_array($col[0], $columnas)) {
+                    echo "<th>" . $col[0] . "</th>";
                 }
-                
             }
-            echo "<th>Acciones</th>";
+            echo "<th><a href='#'>+Peliculas</a></th>";
             echo "</tr>";
+
             while ($row = mysqli_fetch_array($result)) {
+                $id = $row['id_pelicula']; // Cambiar id si es necesario
                 echo "<tr>";
-                foreach ($nameArray as $name) {
-                    if ($name == "caratula") {
-                        echo "<td><img src='" . $row[$name] . "' width='100px'></td>";
+                foreach ($columnas as $col) {
+                    if ($col == "caratula") { // Quitar condicion si no es necesaria
+                        echo "<td><img src='" . $row[$col] . "' width='100px'></td>";
                     } else {
-                        echo "<td>" . $row[$name] . "</td>";
+                        echo "<td>" . $row[$col] . "</td>";
                     }
                 }
                 echo "<td>";
-                echo "<a href='delete.php?id=" . $row['id_pelicula'] . "'><img src='" . $enlaces[0] . "' width='20px' height='20px'></a>" . PHP_EOL;
-                echo "<a href='update.php?id=" . $row['id_pelicula'] . "'><img src='" . $enlaces[1] . "' width='20px' height='20px'></a>" . PHP_EOL;
-                echo "<a href='view.php?id=" . $row['id_pelicula'] . "'><img src='" . $enlaces[2] . "' width='20px' height='20px'></a>" . PHP_EOL;
+                echo "<a href='update.php?id=" . $row[$id] . "'>Editar</a>";
+                echo " | ";
+                echo "<a href='delete.php?id=" . $row[$id] . "'>Eliminar</a>";
+                echo "</td>";
                 echo "</tr>";
             }
+
             echo "</table>";
 
             mysqli_free_result($result);
@@ -64,14 +68,10 @@ function visualizarDatos($link, $tabla)
     <div class="centrar">
         <header>
             <h1>Peliculas</h1>
-            <button type="button" onclick="window.location.href='create.php'">Insertar pelicula</button>
         </header>
         <main>
             <?php
-            // Incluir archivo de configuracion
-            require_once "config.php";
-            visualizarDatos($link, "peliculas");
-            // Cerrar conexion
+            visualizarDatos($link, "peliculas", "id_pelicula", "titulo", "caratula");
             mysqli_close($link);
             ?>
             <main>
