c65efeaddcdadceb5033dd4c80b5bbc23b7cb3e1
Zafiro12 <alexsanta1205@gmail.com>
2023-01-12 09:11:33 +0100

    Hace vista login

--- src/tema_3/login/index.php
+++ src/tema_3/login/index.php
@@ -1,97 +1,120 @@
 <?php
+    session_name("login");
+    session_start();
+
     require_once("sql/config.php");
 
-    if (isset($_POST["login"]))
+
+    if (isset($_SESSION["usuario"]))
     {
-        $error_usuario = $_POST["usuario"] == "";
-        $error_clave = $_POST["clave"] == "";
-        $error_form = $error_usuario || $error_clave;
-
-        if (!$error_form)
+        echo "Logeado";
+    }
+    else
+    {
+        if (isset($_POST["login"]))
         {
-            try
+            $error_usuario = $_POST["usuario"] == "";
+            $error_clave = $_POST["clave"] == "";
+            $error_form = $error_usuario || $error_clave;
+
+            if (!$error_form)
             {
-                $conn = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USERNAME, DB_PASSWORD);
-                
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo "Connected successfully"; 
-            }
-            catch(PDOException $e)
-            {
-                die("Connection failed: " . $e->getMessage());
-            }
-
-            try
-            {
-                $consulta = "select * from usuarios where usuario=? and clave=?";
-                $sentencia=$conexion->prepare($consulta);
-
-                $datos[]=$_POST["usuario"];
-                $datos[]=md5($_POST["clave"]);
-
-                $sentencia->execute($datos);
-
-                if ($sentencia->rowCount()>0)
+                try
                 {
-
+                    $conn = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USERNAME, DB_PASSWORD);
+                    
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    echo "Connected successfully"; 
                 }
-                else
+                catch(PDOException $e)
                 {
-                    $error_usuario = true;
+                    die("Connection failed: " . $e->getMessage());
                 }
-            }
-            catch(PDOException $e)
-            {
-                $sentencia=null;
-                $conexion=null;
-                die("Connection failed: " . $e->getMessage());
+
+                try
+                {
+                    $consulta = "select * from usuarios where usuario=? and clave=?";
+                    $sentencia=$conexion->prepare($consulta);
+
+                    $datos[]=$_POST["usuario"];
+                    $datos[]=md5($_POST["clave"]);
+
+                    $sentencia->execute($datos);
+
+                    if ($sentencia->rowCount()>0)
+                    {
+                        $_SESSION["usuario"] = $datos[0];
+                        $_SESSION["clave"] = $datos[1];
+                        $_SESSION["ultimo_acceso"] = time();
+
+                        header("Location:index.php");
+                        exit();
+                    }
+                    else
+                    {
+                        $error_usuario = true;
+                    }
+                }
+                catch(PDOException $e)
+                {
+                    $sentencia=null;
+                    $conexion=null;
+                    die("Connection failed: " . $e->getMessage());
+                }
             }
         }
-    }
-?>
+    ?>
 
+    <!DOCTYPE html>
+    <html lang="es">
 
-<!DOCTYPE html>
-<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Log In PDO</title>
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log In PDO</title>
-</head>
+    <body>
+        <h1>Log In con PDO</h1>
 
-<body>
-    <h1>Log In con PDO</h1>
+        <form action="index.php" method="post">
+            <div>
+                <label for="usuario">Usuario:</label>
+                <input type="text" name="usuario" id="usuario" value="<?php if (isset($_POST["usuario"])) echo $_POST["usuario"]; ?>" />
+                <?php
+                    if (isset($_POST["usuario"]) && $error_usuario)
+                    {
+                        if ($_POST["usuario"] == "")
+                        {
+                            echo "<span style='color: red;'>*Campo vacío</span>";
+                        }
+                        else
+                        {
+                            echo "<span style='color: red;'>*El usuario no es correcto</span>";
+                        }
+                    }
+                ?>
+            </div>
 
-    <form action="index.php" method="post">
-        <div>
-            <label for="usuario">Usuario:</label>
-            <input type="text" name="usuario" id="usuario" value="<?php if (isset($_POST["usuario"])) echo $_POST["usuario"]; ?>" />
-            <?php
-                if (isset($_POST["usuario"]) && $error_usuario)
-                {
-                    echo "<span style='color: red;'>*Campo vacío</span>";
-                }
-            ?>
-        </div>
+            <div>
+                <label for="clave">Contraseña:</label>
+                <input type="password" name="clave" id="clave" />
+                <?php
+                    if (isset($_POST["clave"]) && $error_clave)
+                    {
+                        echo "<span style='color: red;'>*Campo vacío</span>";
+                    }
+                ?>
+            </div>
 
-        <div>
-            <label for="clave">Contraseña:</label>
-            <input type="password" name="clave" id="clave" />
-            <?php
-                if (isset($_POST["clave"]) && $error_clave)
-                {
-                    echo "<span style='color: red;'>*Campo vacío</span>";
-                }
-            ?>
-        </div>
+            <div>
+                <input type="submit" name="login" value="Log In" />
+                <input type="reset" value="Borrar" />
+            </div>
+        </form>
+    </body>
 
-        <div>
-            <input type="submit" name="login" value="Log In" />
-            <input type="reset" value="Borrar" />
-        </div>
-    </form>
-</body>
-
-</html>
+    </html>
+<?php
+}
--- src/tema_3/login/sql/config.php
+++ src/tema_3/login/sql/config.php
@@ -2,4 +2,4 @@
 define('DB_SERVER', 'db');
 define('DB_USERNAME', 'jose');
 define('DB_PASSWORD', 'josefa');
-define('DB_NAME', 'bd_videoclub');
+define('DB_NAME', 'bd_usuarios');
