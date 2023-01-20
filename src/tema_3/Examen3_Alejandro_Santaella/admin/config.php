679b7d6c4e7f217821c6092b8653ca9a93e1a2a0
Zafiro12 <alexsanta1205@gmail.com>
2023-01-20 08:49:38 +0100

    Corrige ejecutar_consulta

--- src/tema_3/Examen3_Alejandro_Santaella/admin/config.php
+++ src/tema_3/Examen3_Alejandro_Santaella/admin/config.php
@@ -11,18 +11,29 @@ function pagina_error($error)
     exit();
 }
 
-function ejecutar_consulta($consulta,  $array=[])
+function ejecutar_consulta($consulta, $array = [])
 {
     try {
-        $conexion = new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
     } catch (PDOException $e) {
         pagina_error($e->getMessage());
+        return false;
     }
     if (count($array) > 0) {
-        $sentencia = $conexion->prepare($consulta);
-        $sentencia->execute($array);
+        try {
+            $sentencia = $conexion->prepare($consulta);
+            $sentencia->execute($array);
+        } catch (PDOException $e) {
+            pagina_error($e->getMessage());
+            return false;
+        }
         return $sentencia;
     } else {
-        return $conexion->query($consulta);
+        try {
+            return $conexion->query($consulta);
+        } catch (PDOException $e) {
+            pagina_error($e->getMessage());
+            return false;
+        }
     }
 }
