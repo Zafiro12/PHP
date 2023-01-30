8ce0e97729d6c344efe8a93d94735c3851c4c33a
Zafiro12 <alexsanta1205@gmail.com>
2023-01-30 10:21:42 +0100

    fix

---
+++ src/tema_4/actividad_1/App.php
@@ -0,0 +1,19 @@
+<?php
+const URL = "http://localhost/tema_4/actividad_1/servicio/";
+
+function curl()
+{
+    return;
+}
+
+?>
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>API REST App</title>
+</head>
+<body>
+<h1>Lista de productos</h1>
+</body>
+</html>
