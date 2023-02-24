a865093afa8567bbcd7c430620673346156221a3
Zafiro12 <alexsanta1205@gmail.com>
2023-02-24 08:38:13 +0100

    fix

--- src/tema_5/practica_1/js/main.js
+++ src/tema_5/practica_1/js/main.js
@@ -4,6 +4,25 @@ function volver() {
     $("#respuesta").html("");
 }
 
+function addProducto() {
+    $("#respuesta").html(
+        "<h2 style='text-align: center;'>Añadir producto</h2>" +
+            "<form id='addForm'>" +
+            "<label for='cod'>Código</label>" +
+            "<input type='text' id='cod' name='cod' required>" +
+            "<label for='nombre_corto'>Nombre corto</label>" +
+            "<input type='text' id='nombre_corto' name='nombre_corto' required>" +
+            "<label for='nombre'>Nombre</label>" +
+            "<input type='text' id='nombre' name='nombre' required>" +
+            "<label for='descripcion'>Descripción</label>" +
+            "<input type='text' id='descripcion' name='descripcion' required>" +
+            "<label for='PVP'>PVP</label>" +
+            "<input type='number' id='PVP' name='PVP' required>" +
+            "<input type='submit' value='Añadir'>" +
+            "</form>"
+    );
+}
+
 function getProducto(codigo) {
     $.ajax({
         url: SERVER_DIR + "/producto/" + codigo,
@@ -49,20 +68,24 @@ function getProductos() {
                 );
             } else {
                 let html_output =
-                    "<table><tr><th>COD</th><th>Nombre Corto</th><th>PVP</th><th id=\"add\" colspan=\"2\">Añadir producto</th></tr>";
+                    '<table><tr><th>COD</th><th>Nombre Corto</th><th>PVP</th><th id="add" colspan="2">Añadir producto</th></tr>';
                 $.each(data.productos, function (_key, tupla) {
                     html_output += "<tr>";
                     html_output +=
                         '<td class="codigo">' + tupla["cod"] + "</td>";
                     html_output += "<td>" + tupla["nombre_corto"] + "</td>";
                     html_output += "<td>" + tupla["PVP"] + "</td>";
-                    html_output += "<td class=\"editar\">Editar</td><td class=\"borrar\">Borrar</td>";
+                    html_output +=
+                        '<td class="editar">Editar</td><td class="borrar">Borrar</td>';
                     html_output += "</tr>";
                 });
                 html_output += "</table>";
                 $("#productos").html(html_output);
                 $(".codigo").click(function () {
                     $("#respuesta").html(getProducto($(this).html()));
+                });
+                $("#add").click(function () {
+                    $("#respuesta").html(addProducto());
                 });
             }
         })
